8ca2b4cbb6d7e866add5018bda2e236fcb5876fb
Vijay thakur <dev@aronwebsolutions.com>
2022-10-07 15:38:47 +0530

    commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 /// <reference types="vitest" />
 import {defineConfig} from 'vite';
 import hydrogen from '@shopify/hydrogen/plugin';
-
+import netlifyPlugin from '@netlify/hydrogen-platform/plugin'
 export default defineConfig({
-  plugins: [hydrogen()],
+  plugins: [hydrogen(),netlifyPlugin()],
   resolve: {
     alias: [{find: /^~\/(.*)/, replacement: '/src/$1'}],
   },
